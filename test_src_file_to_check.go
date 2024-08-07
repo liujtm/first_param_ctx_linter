7c04766a52e98e58a009524de4886fee6ded35ca
jeeery.liu <jeeery.liu@shopee.com>
2024-08-07 15:26:27 +0800

    v1

--- test_src_file_to_check.go
+++ test_src_file_to_check.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"context"
+	"fmt"
 )
 
 func normalAdd(a, b int) int {
-
 	return a + b
 
 }
 
 func ooook(ctx context.Context, a, b int) int {
+	fmt.Println(ctx)
 	return a + b
 }
